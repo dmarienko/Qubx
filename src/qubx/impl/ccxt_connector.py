69565d86ca6b5832f28777b6047beeb04a41a4cb
dmitry <dmitry.ema@gmail.com>
2024-03-24 18:01:27 +0200

    wip: in progress

--- src/qubx/impl/ccxt_connector.py
+++ src/qubx/impl/ccxt_connector.py
@@ -3,15 +3,41 @@ import ccxt.pro as cxp
 from ccxt.base.decimal_to_precision import ROUND_UP
 from ccxt.base.exchange import Exchange
 
+from threading import Thread, Event, Lock
+from queue import Queue
+# from multiprocessing import Queue #as Queue
+
 
 from qubx.core.basics import Instrument, dt_64
 from qubx.core.strategy import IDataProvider
+
 
 _aliases = {
     'binance.um': 'binanceusdm',
     'binance.cm': 'binancecoinm',
     'kraken.f': 'kreakenfutures'
 }
+
+
+class CommChannel:
+    control: Event
+    queue: Queue
+    name: str
+    lock: Lock
+
+    def __init__(self, name: str):
+        self.name = name
+        self.control = Event()
+        self.queue = Queue()
+        self.lock = Lock()
+
+    def stop(self):
+        if self.control.is_set():
+            self.control.clear()
+
+    def start(self):
+        self.control.set()
+
 
 class CCXT_connector(IDataProvider):
     exch: Exchange
