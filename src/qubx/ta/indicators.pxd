750182383360ba13b39944388684942f520ccb1c
dmitry <dmitry.ema@gmail.com>
2024-08-21 18:55:28 +0200

    chore: adds definitions for indicators

---
+++ src/qubx/ta/indicators.pxd
@@ -0,0 +1,138 @@
+cimport numpy as np
+from qubx.core.series cimport Indicator, IndicatorOHLC, RollingSum, TimeSeries, OHLCV, Bar
+
+cdef class Sma(Indicator):
+    cdef unsigned int period
+    cdef RollingSum summator
+
+    cpdef double calculate(self, long long time, double value, short new_item_started)
+
+cdef class Ema(Indicator):
+    cdef int period
+    cdef np.ndarray __s
+    cdef int __i
+    cdef double alpha
+    cdef double alpha_1
+    cdef unsigned short init_mean 
+    cdef unsigned short _init_stage
+
+    cpdef double calculate(self, long long time, double value, short new_item_started)
+
+cdef class Tema(Indicator):
+    cdef int period
+    cdef unsigned short init_mean 
+    cdef TimeSeries ser0
+    cdef Ema ema1
+    cdef Ema ema2
+    cdef Ema ema3
+    cpdef double calculate(self, long long time, double value, short new_item_started)
+
+cdef class Dema(Indicator):
+    cdef int period
+    cdef unsigned short init_mean 
+    cdef TimeSeries ser0
+    cdef Ema ema1
+    cdef Ema ema2
+
+    cpdef double calculate(self, long long time, double value, short new_item_started)
+
+cdef class Kama(Indicator):
+    cdef int period
+    cdef int fast_span
+    cdef int slow_span
+    cdef double _S1 
+    cdef double _K1 
+    cdef _x_past
+    cdef RollingSum summator
+
+    cpdef double calculate(self, long long time, double value, short new_item_started)
+
+cdef class Highest(Indicator):
+    cdef int period
+    cdef object queue
+    cpdef double calculate(self, long long time, double value, short new_item_started)
+
+cdef class Lowest(Indicator):
+    cdef int period
+    cdef object queue
+    cpdef double calculate(self, long long time, double value, short new_item_started)
+
+cdef class Std(Indicator):
+    cdef int period
+    cpdef double calculate(self, long long time, double value, short new_item_started)
+
+cdef class Pewma(Indicator):
+    cdef public TimeSeries std
+    cdef double alpha, beta
+    cdef int T
+
+    cdef double _mean, _vstd, _var
+    cdef double mean, vstd, var
+    cdef long _i
+
+    cpdef double calculate(self, long long time, double value, short new_item_started)
+
+cdef class PewmaOutliersDetector(Indicator):
+    cdef public TimeSeries upper, lower, outliers, std
+    cdef double alpha, beta, threshold
+    cdef int T
+    cdef str dist
+
+    cdef double student_t_df
+    cdef long _i
+    cdef double mean, vstd, variance
+    cdef double _mean, _vstd, _variance, _z_thr
+
+    cpdef double calculate(self, long long time, double x, short new_item_started)
+
+    cdef double _get_z_thr(self)
+    cdef double _get_alpha(self, double p_t)
+    cdef double _get_mean(self, double x, double alpha_t)
+    cdef double _get_variance(self, double x, double alpha_t)
+    cdef double _get_std(self, double variance, double mean)
+    cdef double _get_p(self, double x)
+
+cdef class Psar(IndicatorOHLC):
+    cdef int _bull
+    cdef double _af
+    cdef double _psar
+    cdef double _lp
+    cdef double _hp
+
+    cdef int bull
+    cdef double af
+    cdef double psar
+    cdef double lp
+    cdef double hp
+
+    cdef public TimeSeries upper
+    cdef public TimeSeries lower
+
+    cdef double iaf
+    cdef double maxaf
+
+    cdef _store(self)
+    cdef _restore(self)
+
+    cpdef double calculate(self, long long time, Bar bar, short new_item_started)
+
+cdef class Atr(IndicatorOHLC):
+    cdef short percentage
+    cdef TimeSeries tr
+    cdef Indicator ma
+
+    cpdef double calculate(self, long long time, Bar bar, short new_item_started)
+
+cdef class Swings(IndicatorOHLC):
+    cdef double _min_l
+    cdef long long _min_t
+    cdef double _max_h
+    cdef long long _max_t
+    cdef OHLCV base
+    cdef Indicator trend
+    # tops contain upper pivot point prices
+    # tops_detection_lag contain time lag when top was actually spotted
+    cdef public TimeSeries tops, tops_detection_lag
+    cdef public TimeSeries bottoms, bottoms_detection_lag
+
+    cpdef double calculate(self, long long time, Bar bar, short new_item_started)
--- src/qubx/ta/indicators.pyx
+++ src/qubx/ta/indicators.pyx
@@ -16,12 +16,7 @@ cdef class Sma(Indicator):
     """
     Simple Moving Average indicator
     """
-    cdef unsigned int period
-    cdef RollingSum summator
 
-    """
-    Simple moving average
-    """
     def __init__(self, str name, TimeSeries series, int period):
         self.period = period
         self.summator = RollingSum(period)
@@ -40,13 +35,6 @@ cdef class Ema(Indicator):
     """
     Exponential moving average
     """
-    cdef int period
-    cdef np.ndarray __s
-    cdef int __i
-    cdef double alpha
-    cdef double alpha_1
-    cdef unsigned short init_mean 
-    cdef unsigned short _init_stage
 
     def __init__(self, str name, TimeSeries series, int period, init_mean=True):
         self.period = period
@@ -92,12 +80,6 @@ def ema(series:TimeSeries, period: int, init_mean: bool = True):
 
 
 cdef class Tema(Indicator):
-    cdef int period
-    cdef unsigned short init_mean 
-    cdef TimeSeries ser0
-    cdef Ema ema1
-    cdef Ema ema2
-    cdef Ema ema3
 
     def __init__(self, str name, TimeSeries series, int period, init_mean=True):
         self.period = period
@@ -118,11 +100,6 @@ def tema(series:TimeSeries, period: int, init_mean: bool = True):
 
 
 cdef class Dema(Indicator):
-    cdef int period
-    cdef unsigned short init_mean 
-    cdef TimeSeries ser0
-    cdef Ema ema1
-    cdef Ema ema2
 
     def __init__(self, str name, TimeSeries series, int period, init_mean=True):
         self.period = period
@@ -142,13 +119,13 @@ def dema(series:TimeSeries, period: int, init_mean: bool = True):
 
 
 cdef class Kama(Indicator):
-    cdef int period
-    cdef int fast_span
-    cdef int slow_span
-    cdef double _S1 
-    cdef double _K1 
-    cdef _x_past
-    cdef RollingSum summator
+    # cdef int period
+    # cdef int fast_span
+    # cdef int slow_span
+    # cdef double _S1 
+    # cdef double _K1 
+    # cdef _x_past
+    # cdef RollingSum summator
 
     def __init__(self, str name, TimeSeries series, int period, int fast_span=2, int slow_span=30):
         self.period = period
@@ -183,8 +160,6 @@ def kama(series:TimeSeries, period: int, fast_span:int=2, slow_span:int=30):
 
 
 cdef class Highest(Indicator):
-    cdef int period
-    cdef queue
 
     def __init__(self, str name, TimeSeries series, int period):
         self.period = period
@@ -214,8 +189,6 @@ def highest(series:TimeSeries, period:int):
 
 
 cdef class Lowest(Indicator):
-    cdef int period
-    cdef queue
 
     def __init__(self, str name, TimeSeries series, int period):
         self.period = period
@@ -246,7 +219,6 @@ def lowest(series:TimeSeries, period:int):
 
 # - - - - TODO !!!!!!!
 cdef class Std(Indicator):
-    cdef int period
 
     def __init__(self, str name, TimeSeries series, int period):
         self.period = period
@@ -285,13 +257,6 @@ cdef double student_t_pdf(double x, double df):
 
 
 cdef class Pewma(Indicator):
-    cdef public TimeSeries std
-    cdef double alpha, beta
-    cdef int T
-
-    cdef double _mean, _vstd, _var
-    cdef double mean, vstd, var
-    cdef long _i
 
     def __init__(self, str name, TimeSeries series, double alpha, double beta, int T):
         self.alpha = alpha 
@@ -359,15 +324,6 @@ def pewma(series:TimeSeries, alpha: float, beta: float, T:int=30):
 
 
 cdef class PewmaOutliersDetector(Indicator):
-    cdef public TimeSeries upper, lower, outliers, std
-    cdef double alpha, beta, threshold
-    cdef int T
-    cdef str dist
-
-    cdef double student_t_df
-    cdef long _i
-    cdef double mean, vstd, variance
-    cdef double _mean, _vstd, _variance, _z_thr
 
     def __init__(
         self,
@@ -498,23 +454,6 @@ def pewma_outliers_detector(
 
 
 cdef class Psar(IndicatorOHLC):
-    cdef int _bull
-    cdef double _af
-    cdef double _psar
-    cdef double _lp
-    cdef double _hp
-
-    cdef int bull
-    cdef double af
-    cdef double psar
-    cdef double lp
-    cdef double hp
-
-    cdef public TimeSeries upper
-    cdef public TimeSeries lower
-
-    cdef double iaf
-    cdef double maxaf
 
     def __init__(self, name, series, iaf, maxaf):
         self.iaf = iaf
@@ -636,9 +575,6 @@ def smooth(TimeSeries series, str smoother, *args, **kwargs) -> Indicator:
 
 
 cdef class Atr(IndicatorOHLC):
-    cdef short percentage
-    cdef TimeSeries tr
-    cdef Indicator ma
 
     def __init__(self, str name, OHLCV series, int period, str smoother, short percentage):
         self.percentage = percentage
@@ -665,16 +601,6 @@ def atr(series: OHLCV, period: int = 14, smoother="sma", percentage: bool = Fals
 
 
 cdef class Swings(IndicatorOHLC):
-    cdef double _min_l
-    cdef long long _min_t
-    cdef double _max_h
-    cdef long long _max_t
-    cdef OHLCV base
-    cdef Indicator trend
-    # tops contain upper pivot point prices
-    # tops_detection_lag contain time lag when top was actually spotted
-    cdef public TimeSeries tops, tops_detection_lag
-    cdef public TimeSeries bottoms, bottoms_detection_lag
 
     def __init__(self, str name, OHLCV series, trend_indicator, **indicator_args):
         self.base = OHLCV("base", series.timeframe, series.max_series_length)
