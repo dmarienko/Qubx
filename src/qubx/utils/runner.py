53bba568d616f19f5c0bb97c72374e8e701216c4
z.m.dev <z.m.dev@outlook.com>
2024-12-19 12:06:15 +0200

    runner refafixctoring

--- src/qubx/utils/runner.py
+++ src/qubx/utils/runner.py
@@ -7,7 +7,6 @@ import time
 import uuid
 from os.path import exists, expanduser
 from pathlib import Path
-from typing import Literal
 
 import click
 import pandas as pd
@@ -68,7 +67,7 @@ def run_ccxt_trading(
     paper: bool = False,
     paper_capital: float = 100_000,
     aux_config: dict | None = None,
-    log: str = Literal["InMemoryLogsWriter", "CsvFileLogsWriter"],
+    log: str = str,
     loop: asyncio.AbstractEventLoop | None = None,
 ) -> StrategyContext:
     strategy_id = strategy_id or uuid.uuid4().hex[:8]  # todo: not sure, but if take from tags cannot distinguish between different runs
